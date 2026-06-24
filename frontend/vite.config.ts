c57da87835c2288844b4975affb666ec81a62c77
yaojie <yaojie@qq.com>
2026-06-24 17:53:16 +0800

    init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,37 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
+    build: {
+        sourcemap: false,
+        outDir: 'dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            maxParallelFileOps: 16,
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) return 'vendor'
+                }
+            }
+        }
     },
-  },
+    optimizeDeps: {
+        force: true
+    },
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+        },
+    },
 })
